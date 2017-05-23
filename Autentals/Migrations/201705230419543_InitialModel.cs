a3efde88d98af1ccd59859e9bb05efe57473fc7c
brianhankins <brian.a.hankins@gmail.com>
2017-05-22 23:46:34 -0500

    Added entity framework

---
+++ Autentals/Migrations/201705230419543_InitialModel.cs
@@ -0,0 +1,41 @@
+namespace Autentals.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Age = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Vehicles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Year = c.Int(nullable: false),
+                        Make = c.String(),
+                        Model = c.String(),
+                        Color = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Vehicles");
+            DropTable("dbo.Customers");
+        }
+    }
+}
---
+++ Autentals/Models/DbModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Autentals.Models
+{
+    public class DbModel : DbContext
+    {
+        public virtual DbSet<Vehicle> Vehicle { get; set; }
+        public virtual DbSet<Customer> Customer { get; set; }
+    }
+}
